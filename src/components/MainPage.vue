f383a3bbd04aa707afa77f2e785a22e7bdf17f16
jlamoreaux <jlamoreaux@bethel.jw.org>
2023-01-08 10:41:23 -0500

    init

---
+++ src/components/MainPage.vue
@@ -0,0 +1,30 @@
+<script setup>
+</script>
+
+<template>
+  <div id="header">
+<table>
+  <tr>
+    <td>
+Home
+    </td>
+    <td>Projects</td>
+    <div class="header-dropdown">
+  <tr>
+    Demos
+  </tr>
+  <tr>Hobbies</tr>
+</div>
+    <td>Contact</td>
+  </tr>
+</table>
+  </div>
+  
+  <div id="footer"></div>
+  
+</template>
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
