192f6757617814e83a6e7e94a66d0bff5b349b0e
jlamoreaux <jlamoreaux@bethel.jw.org>
2023-01-08 11:20:20 -0500

    about me

---
+++ src/components/AboutMe.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <div id="intro">
+    <span>Jachin Lamoreaux</span>
+    <span>Javascript Developer</span>
+  </div>
+
+  <div id="work-history">
+
+  </div>
+  
+</template>
+
+<style scoped>
+</style>
