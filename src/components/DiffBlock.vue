2557dba54c8d35f400971b8bcefad28f2219b2d8
Lamoreaux, Jachin <jlamoreaux@bethel.jw.org>
2023-01-23 17:56:15 -0500

    diff works but doesnt render

---
+++ src/components/DiffBlock.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref } from "vue";
+import diffFuncs from "../js/difference.js";
+
+const difference = diffFuncs();
+
+var diffArr = ref();
+
+function runDiff() {
+  var a = document.getElementById("texta").value;
+  var b = document.getElementById("textb").value;
+
+  diffArr = difference.compareText(a, b);
+
+  console.log(structuredClone(diffArr));
+}
+</script>
+
+<template>
+  <div id="diff-block">
+    <textarea id="texta" class="diff-box"></textarea>
+    <textarea id="textb" class="diff-box"></textarea>
+    <button @click="runDiff()">Diff</button>
+
+    <div>
+      <p id="diff-output">
+        <span v-for="frag in diffArr" :key="frag.UUID" :id="frag.UUID">
+          {{ frag }}
+          Hello
+        </span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,30 +1,29 @@
 <script setup>
+import DiffBlock from "./DiffBlock.vue";
 </script>
 
 <template>
   <div id="header">
-<table>
-  <tr>
-    <td>
-Home
-    </td>
-    <td>Projects</td>
-    <div class="header-dropdown">
-  <tr>
-    Demos
-  </tr>
-  <tr>Hobbies</tr>
-</div>
-    <td>Contact</td>
-  </tr>
-</table>
+    <table>
+      <tr>
+        <td>Home</td>
+        <td>Projects</td>
+        <div class="header-dropdown">
+          <tr>
+            Demos
+          </tr>
+          <tr>
+            Hobbies
+          </tr>
+        </div>
+        <td>Contact</td>
+      </tr>
+    </table>
   </div>
-  
-  <div id="footer">
 
-  </div>
-  
+  <DiffBlock></DiffBlock>
+
+  <div id="footer"></div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
